5f0bf18fc4c3a1d250a3894e93d65a36cb9b0790
gaccheshubham <mailer3261@gmail.com>
2023-01-25 23:19:29 +0530

    unittests

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,11 +1,4 @@
-// // We require the Hardhat Runtime Environment explicitly here. This is optional
-// // but useful for running the script in a standalone fashion through `node <script>`.
-// //
-// // You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
-// // will compile your contracts, add the Hardhat Runtime Environment's members to the
-// // global scope, and execute the script.
-
-const {ethers} = require("hardhat");
+const { ethers } = require("hardhat");
 
 async function main() {
   const [owner, otherAccount] = await ethers.getSigners();
@@ -14,15 +7,11 @@ async function main() {
     "WhitelistedERC1155"
   );
   const whitelistedERC1155 = await WhitelistedERC1155.deploy();
+  await whitelistedERC1155.deployed();
+  console.log(`contract deployed to ${whitelistedERC1155.address}`);
 
-  console.log(
-    `contract deployed to ${whitelistedERC1155.address}`
-  );
-
-  console.log(await whitelistedERC1155.uri(2))
 }
 
 main().catch((error) => {
   console.error(error);
-  process.exitCode = 1;
 });
